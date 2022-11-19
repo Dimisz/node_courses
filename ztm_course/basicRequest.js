2e14ab7b0a53f1169d0419839d5fadaca71f0e58
Vladimir Solovyov <leilyngale@icloud.com>
2022-11-19 13:26:18 +0300

    Alternative syntax for requests

--- ztm_course/basicRequest.js
+++ ztm_course/basicRequest.js
@@ -1,3 +1,5 @@
+// option 1 HTTP
+/*
 const http = require('http');
 
 const req = http.request('http://www.google.com', (res) => {
@@ -10,3 +12,32 @@ const req = http.request('http://www.google.com', (res) => {
 });
 
 req.end();
+*/
+
+// option 2 HTTPS
+/*
+const https = require('https');
+
+const req = https.request('https://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`Data chunk: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log("No more data");
+    });
+});
+
+req.end();
+*/
+
+// option 3 USING DESTRUCTURING
+const { get } = require('https');
+
+get('https://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`Data chunk: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log("No more data");
+    });
+});
