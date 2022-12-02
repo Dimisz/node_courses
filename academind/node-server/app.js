9432cad61c29aacf1145b13c26611d796d7ac916
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-02 08:37:09 +0300

    Basic routing separated into a separate folder

--- academind/node-server/app.js
+++ academind/node-server/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// routes imports
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log("In another middleware");
-    res.send('<h1>Hello from express</h1>');
+    res.status(404).send('<h1>Page not found</h1>');
 });
+
 
 app.listen(3000);
