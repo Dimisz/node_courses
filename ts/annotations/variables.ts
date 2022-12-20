4df37568f363f1b21cb08e9d2fc77f5f9eb47178
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-20 18:45:55 +0800

    Using annotations in TypeScript

---
+++ ts/annotations/variables.ts
@@ -0,0 +1,59 @@
+const apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+// built in objects 
+let now: Date = new Date();
+
+// array
+let colors: string[] = ['red', 'green', 'blue'];
+let nums: number[] = [1, 2, 3, 4];
+let bools: boolean[] = [true, true, false];
+ 
+
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
+
+// functions
+const logNumber: (i: number) => void = (i:number) => {
+  console.log(i);
+};
+
+
+// when to use annotations
+// 1) function that returns 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number} = JSON.parse(json);
+console.log(coordinates);
+
+// 2) when we declare variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+for(let i = 0; i < words.length; i++){
+  if(words[i] === 'green'){
+    foundWord = true;
+  }
+}
+
+
+// 3) variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // two types kinda
+for(let i = 0; i < numbers.length; i++){
+  if(numbers[i] > 0){
+    numberAboveZero = numbers[i];
+  }
+}
